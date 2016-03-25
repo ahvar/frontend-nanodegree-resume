930e87fa66e52066c7f32f3ea662e2f91529cc63
Arthur Vargas <ahvargas92@gmail.com>
2016-03-25 12:56:52 -0400

    removed ajax code and put json objects in resumeBuilder. made display functions global

--- Project2/js/resumeBuilder.js
+++ Project2/js/resumeBuilder.js
@@ -3,6 +3,87 @@
 *Arthur Vargas
 */
 
+var	bio = {
+
+		"name": "Arthur Vargas",
+		"role": "Developer",
+		"contacts":
+
+		{
+
+			"mobile": "336-416-7584",
+			"email": "ahvargas92@gmail.com",
+			"github": "https://github.com/ahvar",
+			"twitter": "@ahvargas",
+			"location": "Raleigh, NC"
+
+		},
+
+		"welcomeMessage": "Welcome to my online resume!",
+		"skills": ["HTML", "CSS", "JavaScript", "Java"],
+		"biopic": "http://www.dsicomedytheater.com/pix/people/Arthur_Vargas.jpg"
+};
+
+var education = {
+
+		"schools": [{
+			"name": "North Carolina State University",
+			"location": "Raleigh, NC",
+			"degree": "BS & BA",
+			"majors": ["English", "Biology"],
+			"dates": "2002 - 2006",
+			"url": "www.ncsu.edu"
+		}],
+		"onlineCourses": [{
+			"title": "JavaScript Basics",
+			"school": "Udacity",
+			"date": "01-2016",
+			"url": "www.udacity.com"
+		}, {
+			"title": "How to Use Git and Github",
+			"school": "Udacity",
+			"date": "12-2015",
+			"url": "www.udacity.com"
+		}, {
+			"title": "Intro to HTML and CSS",
+			"school": "Udacity",
+			"date": "12-2015",
+			"url": "www.udacity.com"
+		}]
+};
+
+var	work = {
+		"jobs": [{
+			"employer": "Reichhold Inc.",
+			"title": "Sales Trainee",
+			"location": "Durham, NC",
+			"dates": "07/2007 - 10/2008",
+			"description": "A global supplier to the Composite & Coating Industry."
+		}, {
+			"employer": "Polyzen Inc.",
+			"title": "Inside Sales Representative",
+			"location": "Apex, NC",
+			"dates": "05/2009 - 08/2014",
+			"description": "A contract manufacturer of disposable medical devices."
+		}, {
+			"employer": "Robling Medical Inc.",
+			"title": "Inside Sales Representative",
+			"location": "Apex, NC",
+			"dates": "02/2015 - 09/2015",
+			"description": "A contract manufacturer of medical products."
+		}]
+};
+var projects = [
+                
+                {
+                	"title": "Portfolio Website",
+                	"dates": "2016",
+                	"description": "A portfolio of development work",
+                	"images": ["C:\\Users\\Arthur\\Pictures\\Screenshots"]
+                }
+                
+              ];
+
 /*variables to contain formatted HTML strings*/
 var formattedheaderName;
 var formattedheaderRole;
@@ -42,159 +123,139 @@ var formattedprojectDates;
 var formattedprojectDescription;
 var formattedprojectImage;
 
-$.ajaxSetup( {"async": false} );
 /*
- * jQuery.getJSON fetches/parses JSON data in resumeDetail.json and passes the resulting object
- * to the callback function 
+ * bio.display function iterates through the bio object in 
+ * resumeDetail.json and appends the information to index.html 
  */
-$.getJSON('resumeDetail.json', function(data) {
+bio.display = function() {
+	"use strict";
 	
-	/*
-	 * jQuery.each function iterates through object properties and arrays contents
-	 * in the data object and calls the respective display functions 
+	//assign formatted HTML to formattedVariable
+	formattedheaderName = HTMLheaderName.replace("%data%",bio.name);
+	formattedheaderRole = HTMLheaderRole.replace("%data%",bio.role);
+	formattedcontactGeneric = HTMLcontactGeneric.replace("%data%",bio.contacts.index);
+	
+	formattedbioPic = HTMLbioPic.replace("%data%",bio.biopic);
+	formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+	formattedskillsStart = HTMLskillsStart.replace("%data%",bio.skills.key);
+	
+	//insert formatted HTML into index.html
+	$("#header").prepend(formattedheaderName);
+	$("#header").prepend(formattedheaderRole);
+	$("#header").append(formattedbioPic, formattedwelcomeMsg);
+	$("#topContacts").append(HTMLskillsStart);
+	
+	var i = 0;
+	while(i < bio.skills.length) {
+		formattedskills = HTMLskills.replace("%data%",bio.skills[i].toString());
+		$("#topContacts").append(formattedskills);
+		$("#footerContacts").append(formattedskills);	
+		i++;
+	}
+	
+	
+	/* The for/each loop iterates through the contacts object and inserts the data
+	 * into the correct HTML string.  The formatted string is assigned to formattedVariable
 	 */
-	$(data).each(function(index,value) {
-		
-		/*
-		 * bio.display function iterates through the bio object in 
-		 * resumeDetail.json and appends the information to index.html 
-		 */
-		data.bio.display = function() {
-			"use strict";
-			
-			//assign formatted HTML to formattedVariable
-			formattedheaderName = HTMLheaderName.replace("%data%",data.bio.name);
-			formattedheaderRole = HTMLheaderRole.replace("%data%",data.bio.role);
-			formattedcontactGeneric = HTMLcontactGeneric.replace("%data%",data.bio.contacts.index);
-			
-			formattedbioPic = HTMLbioPic.replace("%data%",data.bio.biopic);
-			formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%",data.bio.welcomeMessage);
-			formattedskillsStart = HTMLskillsStart.replace("%data%",data.bio.skills.key);
-			
-			//insert formatted HTML into index.html
-			$("#header").prepend(formattedheaderName);
-			$("#header").prepend(formattedheaderRole);
-			$("#header").append(formattedbioPic, formattedwelcomeMsg);
-			$("#topContacts").append(HTMLskillsStart);
-			
-			var i = 0;
-			while(i < data.bio.skills.length) {
-				formattedskills = HTMLskills.replace("%data%",data.bio.skills[i].toString());
-				$("#topContacts").append(formattedskills);
-				$("#footerContacts").append(formattedskills);	
-				i++;
-			}
-			
-			
-			/* The for/in loop iterates through the contacts object and inserts the data
-			 * into the correct HTML string.  The formatted string is assigned to formattedVariable
-			 */
-			for(var info in data.bio.contacts) {
-				if(data.bio.contacts[info].mobile)
-					formattedMobile = HTMLmobile.replace("%data%",data.bio.contacts[info].mobile);
-				else if (data.bio.contacts[info].email)
-					formattedEmail = HTMLemail.replace("%data%",data.bio.contacts[info].email);
-				else if(data.bio.contacts[info].twitter)
-					formattedTwitter = HTMLtwitter.replace("%data%",data.bio.contacts[info].twitter);
-				else if(data.bio.contacts[info].github)
-					formattedGithub = HTMLgithub.replace("%data%",data.bio.contacts[info].github);
-				else if(data.bio.contacts[info].blog)
-					formattedBlog = HTMLblog.replace("%data%",data.bio.contacts[info].blog);
-				else if(data.bio.contacts[info].location)
-					formattedLocation = HTMLlocation.replace("%data%",data.bio.contacts[info].location);				
-			}
-			
-			/*append formatted HTML to index*/
-			$("#topContacts").append(formattedMobile,formattedEmail,formattedGithub,formattedTwitter,formattedLocation);
-			$("#footerContacts").append(formattedMobile,formattedEmail,formattedGithub,formattedTwitter,formattedLocation);	
-		};
-		
-		/*
-		 * The education.display function iterates through the education objects in 
-		 * resumeDetail.json and appends the information to index.html 
-		 */
-		data.education.display = function() {
-			"use strict";
-			for(var i = 0; i < data.education.schools.length; i++) {
-				$("#education").append(HTMLschoolStart);
-				console.log(HTMLschoolName.replace("%data%",data.education.schools[i].name));
-				console.log(data.education.schools[i].degree);
-				console.log(data.education.schools[i].dates);
-				formattedschoolName = HTMLschoolName.replace("%data%",data.education.schools[i].name);
-				formattedschoolDegree = HTMLschoolDegree.replace("%data%", data.education.schools[i].degree);
-				formattedschoolDates = HTMLschoolDates.replace("%data%", data.education.schools[i].dates);
-				formattedschoolLocation = HTMLschoolLocation.replace("%data%", data.education.schools[i].location);
-				$(".education-entry:last").append(formattedschoolName + formattedschoolDegree,formattedschoolDates,
-						formattedschoolLocation);
-				for(var j = 0; j < data.education.schools[i].majors.length; j++) {
-					formattedschoolMajor = HTMLschoolMajor.replace("%data%", data.education.schools[i].majors[j]);
-					$(".education-entry:last").append(formattedschoolMajor);
-				}
-			}
-			
-			for(var i = 0; i < data.education.onlineCourses.length; i++) {
-				$("#education").append(HTMLonlineClasses);
-				formattedonlineTitle = HTMLonlineTitle.replace("%data%", data.education.onlineCourses[i].title);
-				formattedonlineSchool = HTMLonlineSchool.replace("%data%", data.education.onlineCourses[i].school);
-				formattedonlineDate = HTMLonlineDates.replace("%data%", data.education.onlineCourses[i].date);
-				formattedURL = HTMLonlineURL.replace("%data%", data.education.onlineCourses[i].url);
-				$(".education-entry:last").append(formattedonlineTitle + formattedonlineSchool, formattedonlineDate, formattedURL);	
-			}
-		};
-		
-		/*
-		 * The work.display function iterates through the work objects in 
-		 * resumeDetail.json and appends the information to index.html 
-		 */
-		data.work.display = function() {
-			"use strict";
-			for(var i = 0; i < data.work.jobs[i]; i++) {
-				$("#workExperience").append(HTMLworkStart);
-				formattedEmployer = HTMLworkEmployer.replace("%data%",data.work.jobs[i].employer);
-				formattedTitle = HTMLworkTitle.replace("%data%",data.work.jobs[i].title);
-				formattedLocation = HTMLworkLocation.replace("%data%",data.work.jobs[i].location);
-				formattedDates = HTMLworkDates.replace("%data%",data.work.jobs[i].dates);
-				formattedDescription = HTMLworkDescription.replace("%data%",data.work.jobs[i].description);
-				$(".work-entry:last").append(formattedEmployer,formattedTitle,formattedLocation,formattedDates,formattedDescription);
-			}
-		};
-		
-		/*
-		 * The projects.display function iterates through the projects array in 
-		 * resumeDetail.json and appends the information to index.html 
-		 */
-		data.projects.display = function() {
-			"use strict";
-			for(var i = 0; i < data.projects[i]; i++) {
-				$("#projects").append(HTMLprojectStart);
-				formattedprojectTitle = HTMLprojectTitle.replace("%data%", data.projects[i].title);
-				formattedprojectDates = HTMLprojectDates.replace("%data%", data.projects[i].dates);
-				formattedprojectDescription = HTMLprojectDescription.replace("%data%", data.projects[i].description);
-				$(".project-entry:last").append(formattedprojectTitle, formattedprojectDates, formattedprojectDescription);
-				for(var j = 0; j < data.projects[i].images[j].length; j++) {
-					formattedprojectImage = HTMLprojectImage.replace("%data%", data.projects[i].images[j]);
-					$(".project-entry:last").append(formattedImage);
-				}
-			}
-		};
-		
-		data.work.display();
-		data.projects.display();
-		data.education.display();
-		data.bio.display();
-		$("#main").append(internationalizeButton);
-		$("#mapDiv").append(googleMap);
+	$.each(bio.contacts,function(index,value) {
+			formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+			formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+			formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
+			formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+			formattedBlog = HTMLblog.replace("%data%",bio.contacts.blog);
+			formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);				
 	});
+	
+	/*append formatted HTML to index*/
+	$("#topContacts").append(formattedMobile,formattedEmail,formattedGithub,formattedTwitter,formattedLocation);
+	$("#footerContacts").append(formattedMobile,formattedEmail,formattedGithub,formattedTwitter,formattedLocation);	
+};
 
-	function inName(name) {
+
+/*
+ * The education.display function iterates through the education objects in 
+ * resumeDetail.json and appends the information to index.html 
+ */
+education.display = function() {
+	"use strict";
+	for(var i = 0; i < education.schools.length; i++) {
+		$("#education").append(HTMLschoolStart);
+		console.log(HTMLschoolName.replace("%data%",education.schools[i].name));
+		console.log(education.schools[i].degree);
+		console.log(education.schools[i].dates);
+		formattedschoolName = HTMLschoolName.replace("%data%",education.schools[i].name);
+		formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+		formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+		formattedschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+		$(".education-entry:last").append(formattedschoolName + formattedschoolDegree,formattedschoolDates,
+				formattedschoolLocation);
+		for(var j = 0; j < education.schools[i].majors.length; j++) {
+			formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors[j]);
+			$(".education-entry:last").append(formattedschoolMajor);
+		}
+	}
+	
+	for(var i = 0; i < education.onlineCourses.length; i++) {
+		$("#education").append(HTMLonlineClasses);
+		formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
+		formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+		formattedonlineDate = HTMLonlineDates.replace("%data%", education.onlineCourses[i].date);
+		formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url);
+		$(".education-entry:last").append(formattedonlineTitle + formattedonlineSchool, formattedonlineDate, formattedURL);	
+	}
+};
+
+/*
+ * The work.display function iterates through the work objects in 
+ * resumeDetail.json and appends the information to index.html 
+ */
+work.display = function() {
+	"use strict";
+	for(var i = 0; i < work.jobs.length; i++) {
+		$("#workExperience").append(HTMLworkStart);
+		formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
+		formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[i].title);
+		formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[i].location);
+		formattedDates = HTMLworkDates.replace("%data%",work.jobs[i].dates);
+		formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[i].description);
+		$(".work-entry:last").append(formattedEmployer,formattedTitle,formattedLocation,formattedDates,formattedDescription);
+	}
+};
+
+/*
+ * The projects.display function iterates through the projects array in 
+ * resumeDetail.json and appends the information to index.html 
+ */
+projects.display = function() {
+	"use strict";
+	for(var i = 0; i < projects.length; i++) {
+		$("#projects").append(HTMLprojectStart);
+		formattedprojectTitle = HTMLprojectTitle.replace("%data%", projects[i].title);
+		formattedprojectDates = HTMLprojectDates.replace("%data%", projects[i].dates);
+		formattedprojectDescription = HTMLprojectDescription.replace("%data%", projects[i].description);
+		$(".project-entry:last").append(formattedprojectTitle, formattedprojectDates, formattedprojectDescription);
+		for(var j = 0; j < projects[i].images.length; j++) {
+			formattedprojectImage = HTMLprojectImage.replace("%data%", projects[i].images[j]);
+			$(".project-entry:last").append(formattedprojectImage);
+		}
+	}
+};
+	
+work.display();
+projects.display();
+education.display();
+bio.display();
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap);
+
+function inName(name) {
 		console.log(name);
 		var newName = name;
 		newName = newName[0].toUpperCase() + newName.slice(1, newName.indexOf(" ") + 1).toLowerCase() + newName.slice(newName.indexOf(" ") + 1).toUpperCase();
 		return newName;
-	}
+}
 	
-});
-$.ajaxSetup( {"async": true} );
+
 
 	 
 	 		
